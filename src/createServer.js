720d9d7799a47e3b0e31bf628bc2e75193d0a537
tiblazy <hauntedapply@gmail.com>
2025-03-08 11:20:15 -0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,55 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { validationRequest } = require('./convertToCase/validationRequest');
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const validationErrors = [];
+
+    try {
+      const { pathname, searchParams } = new URL(
+        req.url,
+        `http://${req.headers.host}`,
+      );
+
+      const originalText = pathname.slice(1);
+      const targetCase = searchParams.get('toCase')?.toUpperCase();
+
+      validationRequest(originalText, targetCase, validationErrors);
+
+      if (validationErrors.length > 0) {
+        throw new Error('');
+      }
+
+      const { originalCase, convertedText } = convertToCase(
+        originalText,
+        targetCase,
+      );
+
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+
+      res.end(
+        JSON.stringify({
+          originalCase,
+          targetCase,
+          convertedText,
+          originalText,
+        }),
+      );
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 400;
+
+      res.end(
+        JSON.stringify({
+          errors: validationErrors.map((message) => ({
+            message,
+          })),
+        }),
+      );
+    }
+  });
+}
+
+module.exports = { createServer };
